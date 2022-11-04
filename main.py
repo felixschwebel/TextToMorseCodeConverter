9ecfb44b6987fd54b1dc3d5c0feb93b3e63ae4a7
felixschwebel <schwebel.felix@gmail.com>
2022-11-04 18:15:40 +0100

    catch exceptions to avoid crashing

--- main.py
+++ main.py
@@ -2,14 +2,26 @@ from morse_code import GetMorseCode
 
 morse_code = GetMorseCode().morse_code
 
+run = True
 def morser_code_converter():
     translation = ""
     text = input('Please put in a word, phrase or sentence to translate (english characters and numbers only):\n').upper().split()
     for word in text:
         for char in word:
+            try:
                 translation += morse_code[char]
                 translation += '  '
-        translation += ' /  '
+                translation += ' /  '
+            except KeyError:
+                return print('Please only use letters and numbers!')
     print(translation)
+    print('\n+++++++++++++++++++++++++++++++')
+    cont = input('Do you want to translate something else? (y/n)\n')
+    if cont == 'y':
+        morser_code_converter()
+    else:
+        return False
 
-morser_code_converter()
+print('🄼🄾🅁🅂🄴 🄲🄾🄳🄴 🅃🅁🄰🄽🅂🄻🄰🅃🄸🄾🄽')
+while morser_code_converter():
+    morser_code_converter()
