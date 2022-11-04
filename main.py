0d6bf45d515eb3950081282e7c58e9871394e212
felixschwebel <schwebel.felix@gmail.com>
2022-11-04 18:04:58 +0100

    Translation Function

--- main.py
+++ main.py
@@ -3,6 +3,13 @@ from morse_code import GetMorseCode
 morse_code = GetMorseCode().morse_code
 
 def morser_code_converter():
-    text = input('Please put in a word, phrase or sentence to translate (english characters and numbers only):\n').split()
+    translation = ""
+    text = input('Please put in a word, phrase or sentence to translate (english characters and numbers only):\n').upper().split()
+    for word in text:
+        for char in word:
+                translation += morse_code[char]
+                translation += '  '
+        translation += ' /  '
+    print(translation)
 
 morser_code_converter()
