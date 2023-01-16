cb17846451d1f64d3af1df4df66ab0be517bcb27
felixschwebel <schwebel.felix@gmail.com>
2023-01-16 13:53:42 +0100

    restart when error raised

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from morse_code import GetMorseCode
 morse_code = GetMorseCode().morse_code
 
 
-def morser_code_converter():
+def morse_code_converter():
     translation = ""
     text = input('Please put in a word, phrase or sentence to translate (english characters and numbers only):\n').upper().split()
     for word in text:
@@ -12,16 +12,18 @@ def morser_code_converter():
                 translation += morse_code[char]
                 translation += '  '
             except KeyError:
-                return print('Please only use letters and numbers!')
+                print('Please only use letters and numbers!')
+                return morse_code_converter()
         translation += ' /  '
     print(translation)
     print('\n+++++++++++++++++++++++++++++++')
     cont = input('Do you want to translate something else? (y/n)\n')
     if cont == 'y':
-        morser_code_converter()
+        morse_code_converter()
     else:
         return False
 
+
 print('🄼🄾🅁🅂🄴 🄲🄾🄳🄴 🅃🅁🄰🄽🅂🄻🄰🅃🄸🄾🄽')
-while morser_code_converter():
-    morser_code_converter()
+while morse_code_converter():
+    morse_code_converter()
